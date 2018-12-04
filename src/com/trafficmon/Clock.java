f6b6c96c98c3eae3e67ecbc1b45abdd592b300a3
PoojaSridhar16 <32365057+PoojaSridhar16@users.noreply.github.com>
2018-12-04 17:52:54 +0000

    Clock working

--- src/com/trafficmon/Clock.java
+++ src/com/trafficmon/Clock.java
@@ -1,7 +1,43 @@
 package com.trafficmon;
 
-import java.time.LocalTime;
+import org.joda.time.LocalTime;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
-public interface Clock {
-    LocalTime now();
+public class Clock {
+
+    private static Integer fakeHour;
+    private static Integer fakeMinute;
+    private static Integer fakeSecond;
+
+    public static void setFakeTime(Integer hour, Integer minute, Integer second) {
+        fakeHour = hour;
+        fakeMinute = minute;
+        fakeSecond = second;
+    }
+
+    public static Integer getCurrentHour() {
+        if (fakeHour != null) {
+            return fakeHour;
+        } else {
+            return LocalTime.now().getHourOfDay();
+        }
+    }
+
+    public static Integer getCurrentMinute() {
+        if (fakeMinute != null) {
+            return fakeMinute;
+        } else {
+            return LocalTime.now().getMinuteOfHour();
+        }
+    }
+
+    public static Integer getCurrentSecond() {
+        if (fakeSecond != null) {
+            return fakeSecond;
+        } else {
+            return LocalTime.now().getSecondOfMinute();
+        }
+    }
+
 }
